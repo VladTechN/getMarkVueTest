74f685121856859650e9ef89340e0e5156bb66f5
vladexnik <nikityuk.vlad9@gmail.com>
2025-02-07 13:23:16 +0100

    init the app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="greetings">
+      You’ve successfully created a project with Vue
+  </div>
+</template>
+
+<style scoped>
+</style>
